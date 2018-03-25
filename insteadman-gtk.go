8f6b8f50f93419431d112113014223a82c8888f6
jhekasoft <jhekasoft@gmail.com>
2018-03-25 18:34:35 +0300

    GTK. Add list stores

--- insteadman-gtk.go
+++ insteadman-gtk.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"github.com/gotk3/gotk3/gtk"
-	// "log"
 	"fmt"
-	"log"
-	//"os"
+	"os"
+	"./configurator"
+	"./manager"
 )
 
 func main() {
+	c := configurator.Configurator{FilePath: ""}
+	config, e := c.GetConfig()
+
+	m := manager.Manager{Config: config}
+
+	repositories := m.GetRepositories()
+	games, e := m.GetSortedGames()
+	langs := m.FindLangs(games)
+
 	gtk.Init(nil)
 
 	//_, e := gtk.BuilderNewFromFile("./resources/gtk/about.glade")
@@ -21,20 +30,29 @@ func main() {
 		fmt.Printf("E: %v\n", e)
 	}
 
-	obj, err := b.GetObject("window_main")
-	if err != nil {
-		//os.Exit(1)
+	obj, e := b.GetObject("window_main")
+	if e != nil {
+		os.Exit(1)
 	}
-
 	window, ok := obj.(*gtk.Window)
 	if !ok {
-		//os.Exit(1)
+		os.Exit(1)
 	}
 
-	if err != nil {
-		log.Fatal(err.Error())
-		//os.Exit(1)
+	obj, e = b.GetObject("liststore_repo")
+	listStoreRepo, ok := obj.(*gtk.ListStore)
+	for _, repo := range repositories {
+		iter := listStoreRepo.Append()
+		listStoreRepo.Set(iter, []int{0, 1}, []interface{}{repo.Name, repo.Name})
 	}
+
+	obj, e = b.GetObject("liststore_lang")
+	listStoreLang, ok := obj.(*gtk.ListStore)
+	for _, lang := range langs {
+		iter := listStoreLang.Append()
+		listStoreLang.Set(iter, []int{0, 1}, []interface{}{lang, lang})
+	}
+
 
 	window.SetTitle("InsteadMan 3")
 	window.SetDefaultSize(770, 500)
